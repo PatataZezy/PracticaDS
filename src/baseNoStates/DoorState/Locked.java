7673502ed3192337ae6b9ef5b64b26869b8ef0c7
PatataZezy <163458859+PatataZezy@users.noreply.github.com>
2025-11-17 11:42:40 +0100

    Enhance comments in Locked.java
    Added comments to clarify class functionality.

--- src/baseNoStates/DoorState/Locked.java
+++ src/baseNoStates/DoorState/Locked.java
@@ -1,3 +1,6 @@
+// this class assumes all interactions door can make while the actual state is locked, 
+// it has all states variations from locked and a get state to check its own state
+
 package baseNoStates.DoorState;
 
 import baseNoStates.Door;

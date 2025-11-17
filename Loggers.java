801b2f12dfcff751345a344d3da0936bb7102d5e
BernatCortes <162620165+BernatCortes@users.noreply.github.com>
2025-11-17 22:04:10 +0100

    Delete Loggers.java

--- Loggers.java
+++
@@ -1,9 +0,0 @@
-package baseNoStates;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public final class Loggers {
-  public static final Logger logger1 = LoggerFactory.getLogger("milestone1.Webserver");
-  public static final Logger logger2 = LoggerFactory.getLogger("milestone2.Webserver");
-}

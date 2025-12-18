d84b596fbb6f6c93b99bf4a220b32acdeaee84f0
PatataZezy <163458859+PatataZezy@users.noreply.github.com>
2025-12-18 10:54:28 +0100

    m

---
+++ src/lib2/screen_partition.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+class  extends StatefulWidget {
+  const ({super.key});
+
+  @override
+  State<> createState() => _State();
+}
+
+class _State extends State<> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ScreenPartition extends StatefulWidget {
+  const ScreenPartition({super.key});
+
+  @override
+  State<ScreenPartition> createState() => _ScreenPartitionState();
+}
+
+class _ScreenPartitionState extends State<ScreenPartition> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- src/lib2/tree.dart
+++ src/lib2/tree.dart
@@ -1,3 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial_acs/tree.dart';
+
+class ScreenPartition extends StatefulWidget {
+  const ScreenPartition({super.key});
+
+  @override
+  State<ScreenPartition> createState() => _ScreenPartitionState();
+}
+
+class _ScreenPartitionState extends State<ScreenPartition> {
+  late Tree tree;
+
+  @override
+  void initState() {
+    super.initState();
+    tree = getTree("building");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(tree.root.id),
+        actions: <Widget>[
+          IconButton(icon: const Icon(Icons.home),
+            onPressed: () {}
+          // TODO go home page = root
+        ),
+        //TODO other actions
+      ],
+    ),
+    body: ListView.separated(
+      // it's like ListView.builder() but better
+      // because it includes a separator between items
+      padding: const EdgeInsets.all(16.0),
+      itemCount: tree.root.children.length,
+      itemBuilder: (BuildContext context, int index) =>
+          _buildRow(tree.root.children[index], index),
+      separatorBuilder: (BuildContext context, int index) =>
+      const Divider(),
+    ),
+  );
+}
+}
+
+Widget _buildRow(Area area, int index) {
+  assert (area is Partition || area is Space);
+  if (area is Partition) {
+    return ListTile(
+      title: Text('P ${area.id}'),
+      onTap: () => {},
+      // TODO, navigate down to show children areas
+    );
+  } else {
+    return ListTile(
+      title: Text('S ${area.id}'),
+      onTap: () => {},
+      // TODO, navigate down to show children doors
+    );
+  }
+}
+
 abstract class Area{
   late String id;
   late List<dynamic> children;

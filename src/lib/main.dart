d89b603272b2460bef39e36075eb2239545e3bfa
PatataZezy <163458859+PatataZezy@users.noreply.github.com>
2026-01-07 12:11:33 +0100

    main cambio

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         ),
         // see https://docs.flutter.dev/cookbook/design/themes
       ),
-      home: const ScreenBlank(),
+      home: const ScreenPartition(id: 'building')
     );
   }
 }
